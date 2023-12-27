c561e18d97ba6608fcda2468a0fbe2e082cdbd99
sooshiance <s.katafract@gmail.com>
2023-12-27 22:08:30 +0400

    some changes

--- basics.py
+++ basics.py
@@ -178,9 +178,9 @@
 ################################################ Dictionary ################################################
 
 
-Employee = {"Name": "Johnny", "Age": 32, "salary":26000,"Company":"^TCS"}        
-print(type(Employee))        
-print("printing Employee data .... ")        
+Employee = {"Name": "Johnny", "Age": 32, "salary":26000,"Company":"^TCS"}
+print(type(Employee))
+print("printing Employee data .... ")
 print(Employee)  
 
 
